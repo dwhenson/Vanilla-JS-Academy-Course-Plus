0f850b1a0cc5312a6b6355eb6ca8d4858c03b535
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-07-29 20:50:00 +0300

    Add final commented and refactored solution - Update word count

--- Word Counts/main.js
+++ Word Counts/main.js
@@ -1,25 +1,41 @@
-// CHECK why does a global variable increase every time?
-// CHECK what on earth is that function actually doing??
-
 // avoid global scope
 (function () {
   /* ==========  Variables  ========== */
+  // <textarea> where the inputs are typed
   const text = document.querySelector('#text');
+  // <span> to insert the character count
   const characterCount = document.querySelector('#character-count');
+  // <span> to insert the word count
   const wordCount = document.querySelector('#word-count');
-  const checkWords = document.querySelector('.checkWords');
+  // <section> to insert word frequency
   const results = document.querySelector('.results');
+  // the variable that the array of complete words is assigned to
+  let words; //
 
   /* ==========  Functions  ========== */
-  function countWords() {
-    const words = text.value //
-      .split(/[\n\r\s]+/g) //
-      .filter(function (word) {
-        return word.length > 0;
-      });
-    wordCount.textContent = words.length;
+  /**
+   * render values from array to HTML
+   * @param  {object} element the element to insert values
+   * @param  {array} array   the array of values to insert
+   * @return {string}         the HTML to render
+   */
+  function renderCommonWords(element, array) {
+    element.innerHTML = `The most commmon words you write are:
+        ${array
+          .map(function (word) {
+            return `
+          <p><strong>${word[0]}</strong>: ${word[1]} times</p>
+         `;
+          })
+          .join('')}`;
   }
 
+  /**
+   * order nested arrays based on second value
+   * @param  {string} a the last word typed
+   * @param  {string} b the other words in the array
+   * @return {array}   the reordered array
+   */
   function rankWordCount(a, b) {
     if (a[1] === b[1]) {
       return 0;
@@ -27,62 +43,60 @@
     return a[1] > b[1] ? -1 : 1;
   }
 
-  function renderCommonWords(element, array) {
-    element.innerHTML = `Your most commmon words are:
-        ${array
-          .map(function (word) {
-            return `
-          <p><strong>${word[0]}</strong> which you wrote ${word[1]} times</p>
-         `;
-          })
-          .join('')}`;
+  /**
+   * count the number of times each word is written
+   */
+  function countCommonWords() {
+    const count = {};
+    words.forEach(function (word) {
+      // add word to object and increment by one each time it is written
+      count[word.toLowerCase()] = (count[word.toLowerCase()] || 0) + 1;
+      console.log(count);
+    });
+
+    // convert object to array to enable sorting by values
+    const countArray = Object.entries(count);
+    // sort array by count of words (descending order)
+    countArray.sort(rankWordCount);
+    // render sorted array into HTML
+    renderCommonWords(results, countArray);
   }
 
-  function countCommonWords() {
-    const words = text.value //
+  /**
+   * count the number of words in the <textarea>
+   * @return {number} the number of words typed
+   */
+  function countWords() {
+    wordCount.textContent = words.length;
+  }
+
+  /**
+   * update the words variable with complete words
+   * @return {array} an array of complete words (separated by spaces or returns)
+   */
+  function updateWords() {
+    words = text.value //
       .split(/[\n\r\s]+/g) //
       .filter(function (word) {
         return word.length > 0;
       });
-
-    const count = {};
-    words.forEach(function (word) {
-      count[word] = (count[word] || 0) + 1;
-    });
-
-    const countArray = Object.entries(count);
-    countArray.sort(rankWordCount);
-    renderCommonWords(results, countArray);
   }
 
+  /**
+   * the count of the number of characters typed
+   * @return {number} the number of characters
+   */
   function countCharacters() {
     characterCount.textContent = text.value.length;
   }
 
   function inputHandler() {
     countCharacters();
+    updateWords();
     countWords();
-    clickHandler();
-  }
-
-  function clickHandler() {
     countCommonWords();
   }
 
   /* ==========  Inits and Event Listeners  ========== */
   text.addEventListener('input', inputHandler);
-  // checkWords.addEventListener('click', clickHandler);
 })();
-
-function getTable(data) {
-  let result = ['<table border=1>'];
-  for (let row of data) {
-    result.push('<tr>');
-    for (let cell of row) {
-      result.push(`<td>${cell}</td>`);
-    }
-    result.push('</tr>');
-  }
-  result.push('</table>');
-  return result.join('\n');
-}
