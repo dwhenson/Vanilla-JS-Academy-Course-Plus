18d9b34a34a03cb9e3cd70d81427a36b8b971dc3
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-07-29 14:22:42 +0300

    Add first working solution - Update word count First draft solution rendering most common words to HTML; requires refactor and improvements to HTML rendered

--- Word Counts/main.js
+++ Word Counts/main.js
@@ -1,11 +1,5 @@
-// GOAL Modify the Word Count project to show the user what words they use most frequently, and how often they’re used.
-//
-// create object words, word:count
-// loop through words array
-// for each word check if already in object, if so count++
-// else add word to object set count as 1
-// render object to HTML - as table?
-//
+// CHECK why does a global variable increase every time?
+// CHECK what on earth is that function actually doing??
 
 // avoid global scope
 (function () {
@@ -14,6 +8,7 @@
   const characterCount = document.querySelector('#character-count');
   const wordCount = document.querySelector('#word-count');
   const checkWords = document.querySelector('.checkWords');
+  const results = document.querySelector('.results');
 
   /* ==========  Functions  ========== */
   function countWords() {
@@ -25,6 +20,24 @@
     wordCount.textContent = words.length;
   }
 
+  function rankWordCount(a, b) {
+    if (a[1] === b[1]) {
+      return 0;
+    }
+    return a[1] > b[1] ? -1 : 1;
+  }
+
+  function renderCommonWords(element, array) {
+    element.innerHTML = `Your most commmon words are:
+        ${array
+          .map(function (word) {
+            return `
+          <p><strong>${word[0]}</strong> which you wrote ${word[1]} times</p>
+         `;
+          })
+          .join('')}`;
+  }
+
   function countCommonWords() {
     const words = text.value //
       .split(/[\n\r\s]+/g) //
@@ -32,20 +45,15 @@
         return word.length > 0;
       });
 
-    const commonWords = {};
-    words.forEach(function (key) {
-      commonWords[key.toLowerCase()] = (commonWords[key.toLowerCase()] || 0) + 1;
+    const count = {};
+    words.forEach(function (word) {
+      count[word] = (count[word] || 0) + 1;
     });
 
-    const commonWordsArray = Object.entries(commonWords);
-    for (const [word, count] of commonWordsArray) {
-      console.log(`Word: ${word} Count: ${count}`);
-    }
+    const countArray = Object.entries(count);
+    countArray.sort(rankWordCount);
+    renderCommonWords(results, countArray);
   }
-
-  // CHECK why does a global variable increase every time?
-  // CHECK what on earth is that function actually doing??
-  // TODO work out how to render into the DOM!
 
   function countCharacters() {
     characterCount.textContent = text.value.length;
@@ -54,6 +62,7 @@
   function inputHandler() {
     countCharacters();
     countWords();
+    clickHandler();
   }
 
   function clickHandler() {
@@ -62,5 +71,18 @@
 
   /* ==========  Inits and Event Listeners  ========== */
   text.addEventListener('input', inputHandler);
-  checkWords.addEventListener('click', clickHandler);
+  // checkWords.addEventListener('click', clickHandler);
 })();
+
+function getTable(data) {
+  let result = ['<table border=1>'];
+  for (let row of data) {
+    result.push('<tr>');
+    for (let cell of row) {
+      result.push(`<td>${cell}</td>`);
+    }
+    result.push('</tr>');
+  }
+  result.push('</table>');
+  return result.join('\n');
+}
