4a70af69301a9f589e0cea15996529f23ffa575d
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-08-02 15:41:09 +0300

    Final working solution - Giphy API Could be improved by adding text if zero results

--- 03. Fetch APIs - Days 13 - 16/main.js
+++ 03. Fetch APIs - Days 13 - 16/main.js
@@ -3,24 +3,43 @@
   /* ==========  Variables  ========== */
   const button = document.querySelector('button');
   const results = document.querySelector('.outputs');
+  const searchTerm = document.querySelector('.searchTerm');
   const search = document.querySelector('#search');
 
   /* ==========  Functions  ========== */
+
+  /**
+   * render the fetched data to the HTML
+   * @param  {array} data the array of GIFs fetch from the endpoint
+   * @return {string}      the HTML to render
+   */
   function displayGifs(data) {
+    searchTerm.innerHTML = `<h2>Results for <em>${search.value}</em>`;
     results.innerHTML = data
-      .map((source) => `<img src="${source.images.downsized.url}" alt="">`)
+      .map(
+        (source) => `
+        <a href="${source.url}">
+        <img src="${source.images.downsized.url}" alt="${source.title}">
+        </a>`
+      )
       .join('');
     search.value = '';
     search.focus();
   }
 
+  /**
+   * render error to the HTML
+   */
   function renderError() {
-    results.textContent = 'Sorry we messed up';
+    searchTerm.innerHTML = `<h2>You searched for <em>${search.value}</em>. But we messed up. Sorry.</h2>
+      <div style="width:100%;height:0;padding-bottom:82%;position:relative;"><iframe src="https://giphy.com/embed/RFDXes97gboYg" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/angeles-bachcaps-bearfighting-RFDXes97gboYg">via GIPHY</a></p>`;
   }
 
+  /**
+   * fetch the data from the endpoint, based on search parameters
+   */
   function fetchGifs() {
     const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=uEcLu0Uc8xF59YqRLBcDvIOmVwl019nS&q=${search.value}&limit=25&offset=0&rating=g&lang=en`;
-    console.log(endpoint);
     fetch(endpoint)
       .then(function (response) {
         return response.ok ? response.json() : Promise.reject(response);
@@ -36,6 +55,6 @@
 
   /* ==========  Inits and Event Listeners  ========== */
 
-  // search.addEventListener('input', generateSearch);
+  search.focus();
   button.addEventListener('click', fetchGifs);
 })();
