8cac0800b816c6f5a820a282fa12aa46f6080984
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-08-03 20:56:18 +0300

    First working solution APIs and Map Only provides top stories - no categories

---
+++ 04. Fetch APIs and Map/main.js
@@ -0,0 +1,51 @@
+// avoid global scope
+(function(){
+/* ==========  Variables  ========== */
+const app = document.querySelector('#app');
+const endpoint = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=';
+const apiKey = 'IVkW1nLsL3ufkeo8FjJrw6oXLUo7qZ62';
+
+/* ==========  Functions  ========== */
+
+
+function renderError() {
+  app.innerHTML = `
+    <p>Sorry we messed up</p><p>You can <a href="https://www.nytimes.com">read the NYT</a> instead though</p>`
+}
+
+function renderStories (articles) {
+  app.innerHTML = articles.map (function (story){
+    return `
+    <h2><a href="${story.url}">${story.title}</a></h2>
+      <h3>${story.byline}</h3>
+      <p>${story.abstract}</p>
+      <figure>
+        <img
+        src="${story.media[0] ? story.media[0]['media-metadata'][2].url : ''}"
+        alt=""
+        height="${story.media[0] ? 294 : 0}"
+        width="${story.media[0] ? 440 : 0}">
+          <figcaption>
+          ${story.media[0] ? story.media[0].caption : ''}
+          </figcaption>
+      </figure>
+    `}).join('');
+}
+
+function fetchStories () {
+  fetch(endpoint + apiKey)
+    .then(function (response) {
+      return response.ok ? response.json() : Promise.reject(response);
+      })
+      .then(function (data) {
+        renderStories(data.results);;
+      })
+      .catch(function () {
+        renderError();
+      });
+}
+
+/* ==========  Inits and Event Listeners  ========== */
+ fetchStories();
+
+})()
