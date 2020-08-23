b85a78af153ff6164ca5f46d02d86906e671ad3d
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-08-23 20:17:35 +0300

    📦 ADD: Options for message and icon

--- 08. Promises Days 36 - 38/main.js
+++ 08. Promises Days 36 - 38/main.js
@@ -21,30 +21,6 @@
 
 	/* ==========  Functions  ========== */
 
-	/**
-	 * Handle any errors during the fetches
-	 * @param   {Object}  error  The type of error
-	 * @return  {String}         The error message to render
-	 */
-	function handleError(error) {
-		console.log(error);
-		app.innerHTML = `<h2>We're sorry. Something went wrong fetching your weather information.</h2>`;
-	}
-
-	function includeIcon(settings, weatherData) {
-		return settings.icon ? `<img src="icons/${weatherData.weather.icon}.png" alt=""/>` : ``;
-	}
-
-	/**
-	 * Render required information from an object to HTML
-	 * @param   {Object}  weather  The weatherData object returned by getWeather
-	 * @return  {String}           The HTML to render
-	 */
-	function renderHTML(settings, weatherData) {
-		app.innerHTML = `
-			 <div>${includeIcon(settings, weatherData)}</div>`;
-	}
-
 	function weatherPlugin(options, weatherData) {
 		// default options
 		const defaults = {
@@ -57,6 +33,28 @@
 		renderHTML(settings, weatherData);
 	}
 
+	function updateMessage(settings, weatherData) {
+		console.log(weatherData);
+		return settings.message
+			.replace("{{temp}}", weatherData.temp)
+			.replace("{{conditions}}", weatherData.weather.description)
+			.replace("{{city}}", weatherData.city_name);
+	}
+
+	function includeIcon(settings, weatherData) {
+		return settings.icon ? `<img src="icons/${weatherData.weather.icon}.png" alt=""/>` : ``;
+	}
+	/**
+	 * Render required information from an object to HTML
+	 * @param   {Object}  weather  The weatherData object returned by getWeather
+	 * @return  {String}           The HTML to render
+	 */
+	function renderHTML(settings, weatherData) {
+		app.innerHTML = `
+			<div>${includeIcon(settings, weatherData)}</div>
+			<h2>${updateMessage(settings, weatherData)}</h2>`;
+	}
+
 	/**
 	 * Get fetch a users location and call the weatherbit API based on first values fetched
 	 * @return  {Object}  The object returned by the weatherbit API fetch
@@ -67,6 +65,16 @@
 		const weatherResponse = await fetch(`${weatherEndpoint}city=${locationData.city}&key=${weatherApiKey}`);
 		const weatherData = await weatherResponse.json();
 		weatherPlugin(options, weatherData.data[0]);
+	}
+
+	/**
+	 * Handle any errors during the fetches
+	 * @param   {Object}  error  The type of error
+	 * @return  {String}         The error message to render
+	 */
+	function handleError(error) {
+		console.log(error);
+		app.innerHTML = `<h2>We're sorry. Something went wrong fetching your weather information.</h2>`;
 	}
 
 	getWeather({ icon: true }).catch(handleError);
