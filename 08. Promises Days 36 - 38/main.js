35b7f25ebf374a850b493af6f04f3d2c3357942c
Dave Henson <50716370+Sampajanno@users.noreply.github.com>
2020-08-23 15:08:45 +0300

    👍 IMPROVE: Begin work on plugin options
    Icon options functioning OK.

--- 08. Promises Days 36 - 38/main.js
+++ 08. Promises Days 36 - 38/main.js
@@ -1,16 +1,38 @@
+// GOAL modify our weather app script to convert it into a plugin.
+// Pass in their own selector to render the weather into.
+// Decide whether to show temperatures in Fahrenheit of Celsius.
+// Change what message is shown (ex. It's currently {temperature} and {conditions} in {location}).
+// Enable or disable the icon for the weather conditions.
+// STEPS:
+// 1) Remove IIFE and replace with function (options)
+// 2) Create an object of default options for user choices
+// 3) Use Object.assign to merge user choices into default
+// 4) Add options object and key value pairs for choices to function call
+// 5) Replace hard coded text with new variables from object (e.g. message text, degrees etc)
+
 // Avoid global scope
 (function () {
 	/* ==========  Variables  ========== */
-	const app = document.querySelector("#app");
+	// fixed parameters
 	const weatherApiKey = "c81e60446f394ac3b6efb4b5c187cafa";
 	const locationEndpoint = "https://ipapi.co/json/";
 	const weatherEndpoint = "https://api.weatherbit.io/v2.0/current?";
+	const app = document.querySelector("#app");
 
 	/* ==========  Functions  ========== */
 
-	function handleError(err) {
-		console.log(err);
-		app.innerHTML = `<h2>We're sorry. Something went wrong fetching your weather information</h2>`;
+	/**
+	 * Handle any errors during the fetches
+	 * @param   {Object}  error  The type of error
+	 * @return  {String}         The error message to render
+	 */
+	function handleError(error) {
+		console.log(error);
+		app.innerHTML = `<h2>We're sorry. Something went wrong fetching your weather information.</h2>`;
+	}
+
+	function includeIcon(settings, weatherData) {
+		return settings.icon ? `<img src="icons/${weatherData.weather.icon}.png" alt=""/>` : ``;
 	}
 
 	/**
@@ -18,25 +40,35 @@
 	 * @param   {Object}  weather  The weatherData object returned by getWeather
 	 * @return  {String}           The HTML to render
 	 */
-	function renderHTML(weather) {
+	function renderHTML(settings, weatherData) {
 		app.innerHTML = `
-			<img src="icons/${weather.weather.icon}.png" alt="${weather.weather.description}" />
-			<h2>The weather in ${weather.city_name} is ${weather.weather.description.toLowerCase()} right now
-			and it's ${weather.temp} &degC</h2>`;
+			 <div>${includeIcon(settings, weatherData)}</div>`;
+	}
+
+	function weatherPlugin(options, weatherData) {
+		// default options
+		const defaults = {
+			message: "It is currently {{temp}} degrees {{units}} with {{conditions}} in {{city}}",
+			units: "celsius",
+			icon: true,
+			error: "Sorry, we can't get the weather for you right now",
+		};
+		const settings = Object.assign(defaults, options);
+		renderHTML(settings, weatherData);
 	}
 
 	/**
 	 * Get fetch a users location and call the weatherbit API based on first values fetched
 	 * @return  {Object}  The object returned by the weatherbit API fetch
 	 */
-	async function getWeather() {
+	async function getWeather(options) {
 		const locationResponse = await fetch(locationEndpoint);
 		const locationData = await locationResponse.json();
 		const weatherResponse = await fetch(`${weatherEndpoint}city=${locationData.city}&key=${weatherApiKey}`);
 		const weatherData = await weatherResponse.json();
-		renderHTML(weatherData.data[0]);
+		weatherPlugin(options, weatherData.data[0]);
 	}
 
-	/* ==========  Inits and Event Listeners  ========== */
-	getWeather().catch(handleError);
+	getWeather({ icon: true }).catch(handleError);
+	// Avoid global scope
 })();
