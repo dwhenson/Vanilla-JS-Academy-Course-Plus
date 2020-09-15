5f325e66dc917a1df73a5253a419e436010b88bc
Dave Henson <dave.henson@gmail.com>
2020-09-15 16:26:30 +0300

    ⚡ADD: set up files for state exercises

---
+++ 11. State Days 57 onwards - 57/main.js
@@ -0,0 +1,11 @@
+// GOAL:
+// Use state-based UI to create the timer, and countdown from 60 seconds to 0 once a second.
+// When the timer is done, stop the countdown and provide a way for users to start it again.
+
+// STEPS:
+// ...
+
+// Avoid global scope
+(function () {
+	console.log("It works");
+})();
